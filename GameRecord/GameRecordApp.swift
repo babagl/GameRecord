0d222c4cde4c262d4204cd725e4e7e68c4a35eae
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-02-08 08:05:50 +0000

    Initial Commit

---
+++ GameRecord/GameRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameRecordApp.swift
+//  GameRecord
+//
+//  Created by Abdoulaye Aliou SALL on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct GameRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
